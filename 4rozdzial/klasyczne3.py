ee10a7f0582c5ad0313e01204778e5c0b25f23de
nazario22812 <76971123+nazario22812@users.noreply.github.com>
2023-10-25 13:42:05 +0200

    py

--- 4rozdzial/klasyczne3.py
+++ 4rozdzial/klasyczne3.py
@@ -1,8 +1,32 @@
 list = []
 
+# list1 = []
+# list2 = [] 
+count = 0
 while True:
     n = int(input("> "))
+    
     if n == 0:
         break
     else:
-        pass
+        list.append(n)
+
+for i in list:
+    count += 1
+
+if count % 2 == 0:
+    print(f"ilosc parzysta: {count}")
+else:
+    print(f"ilosc nieparzysta: {count}")
+
+for i in range(count):
+    pass    
+    
+    
+# print(f"{list[:int(count1)]}\n{list[int(count1):]}")
+
+# if list[:int(count1)].count != list[int(count1):].count:
+#     print("tablican nie jest symetryczna")
+# else:
+#     print("tablica jest symetryczna")
+ 
